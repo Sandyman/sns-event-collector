84c6d6b870e4c3c60ee7d0589a27098656946510
Sander Huijsen <sander.huijsen@gmail.com>
2017-10-04 16:25:01 +0800

    Some refactoring.

--- index.js
+++ index.js
@@ -7,25 +7,26 @@ const sns = new AWS.SNS();
  * takes an eventCollector and a callback function as parameters.
  * @param topicArn
  */
-const createLogger = (topicArn) => (eventCollector, callback) => {
-    const msg = eventCollector.getEvent();
-
-    // Fail silently when no ARN is configured
+const createLogger = (topicArn) => {
     if (!topicArn) {
-        console.log('No LOG_TOPIC_ARN defined');
-        return callback(null, 'OK');
+        console.log('Warning! No SNS topic ARN provided.');
+        return (eventCollector, callback) => {
+            if (callback) return callback(null, 'OK');
+        }
     }
 
-    const params = {
-        Message: JSON.stringify(msg),
-        TopicArn: topicArn,
-    };
-    // eslint-disable-next-line consistent-return
-    return sns.publish(params, err => {
-        if (err) console.log(`Oops. Something went wrong:\n${err}`);
+    return (eventCollector, callback) => {
+        const params = {
+            Message: JSON.stringify(eventCollector.getEvent()),
+            TopicArn: topicArn,
+        };
+        // eslint-disable-next-line consistent-return
+        return sns.publish(params, err => {
+            if (err) console.log(`Oops. Something went wrong:\n${err}`);
 
-        if (callback) return callback(null, 'OK');
-    });
+            if (callback) return callback(null, 'OK');
+        });
+    };
 };
 
 module.exports = createLogger;
